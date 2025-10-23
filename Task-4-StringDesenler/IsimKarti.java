10857213a8aafa9c80fa144d8e27be5c5f88a212
caganerdogan <250541059@firat.edu.tr>
2025-10-23 15:50:51 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,16 @@
+public class IsimKarti {
+    public static void main(String[] args) {
+        String ad = "Ali";
+        String soyad = "Yılmaz";
+        String ogrenciNo = "202312345";
+        String bolum = "Bilgisayar Mühendisliği";
+        
+        System.out.println("======================================");
+        System.out.println("           ÖĞRENCİ BİLGİ KARTI        ");
+        System.out.println("======================================");
+        System.out.println("Ad Soyad:\t" + ad + " " + soyad);
+        System.out.println("Öğrenci No:\t" + ogrenciNo);
+        System.out.println("Bölüm:\t\t" + bolum);
+        System.out.println("======================================");
+    }
+}
