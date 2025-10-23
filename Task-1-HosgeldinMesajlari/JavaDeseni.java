a4fdc8e37a64a3ad665e3c192ce5308a493acb48
caganerdogan <250541059@firat.edu.tr>
2025-10-23 15:34:48 +0300

    Update JavaDeseni.java

---
+++ Task-1-HosgeldinMesajlari/JavaDeseni.java
@@ -0,0 +1,11 @@
+public class JavaDesen {
+    public static void main(String[] args) {
+        System.out.println("   JAVA   ");
+        System.out.println("  J A V A ");
+        System.out.println(" J A V A J");
+        System.out.println("JAVAJAVAJA");
+        System.out.println(" J A V A J");
+        System.out.println("  J A V A ");
+        System.out.println("   JAVA   ");
+    }
+}
