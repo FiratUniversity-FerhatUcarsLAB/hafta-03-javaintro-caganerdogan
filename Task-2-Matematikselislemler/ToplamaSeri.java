32015af0928fcc6d616ba832b8f314989921c91e
caganerdogan <250541059@firat.edu.tr>
2025-10-23 15:36:58 +0300

    Update ToplamaSeri.java

---
+++ Task-2-Matematikselislemler/ToplamaSeri.java
@@ -0,0 +1,9 @@
+public class ToplamaSerisi {
+    public static void main(String[] args) {
+        // Değişkenler ve toplama operatörü kullanarak hesaplama
+        int toplam = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9;
+        
+        System.out.println("Hesaplanan Seri: 1 + 2 + ... + 9");
+        System.out.println("Toplam Sonucu: " + toplam); // Sonuç: 45
+    }
+}
