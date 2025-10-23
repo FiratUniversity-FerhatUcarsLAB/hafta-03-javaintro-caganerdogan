2f0a08c96ccb333b9c8cdf4b6da53c7c144a9d9c
caganerdogan <250541059@firat.edu.tr>
2025-10-23 15:51:44 +0300

    Update AsciiArtHarf.java

---
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -0,0 +1,10 @@
+public class ASCIIArt {
+    public static void main(String[] args) {
+        System.out.println("--- Kendi Adınızın Baş Harfi (Örnek: A) ---");
+        System.out.println("      /\\    ");
+        System.out.println("     /  \\   ");
+        System.out.println("    /____\\  ");
+        System.out.println("   /      \\ ");
+        System.out.println("  /        \\");
+    }
+}
